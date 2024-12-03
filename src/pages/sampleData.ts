d56780da6a54177bf0377c6c49c672d4d711eccc
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2024-12-03 06:42:25 +0100

    chore: place sample data in a separate file

---
+++ src/pages/sampleData.ts
@@ -0,0 +1,64 @@
+import { IFlightResponse } from "@/lib/api";
+
+export const sampleFLightsResponse: IFlightResponse = {
+  total: 3,
+  count: 1,
+  resources: [
+    {
+      id: "e9890ca4-2bac-4e5b-b614-732176270bad",
+      img: "https://robohash.org/e9890ca4-2bac-4e5b-b614-732176270bad/?size=200x200",
+      status: "none",
+      code: "AbcDef",
+      capacity: 50,
+      departureDate: "2020-10-23",
+    },
+    {
+      id: "e9890ca4-2bac-4e5b-b614-732176270bef",
+      img: "https://robohash.org/e9890ca4-2bac-4e5b-b614-732176270bef/?size=200x200",
+      status: "none",
+      code: "AbcDef",
+      capacity: 50,
+      departureDate: "2020-10-23",
+    },
+    {
+      id: "e9890ca4-2bac-4e5b-b614-732176270bed",
+      img: "https://robohash.org/e9890ca4-2bac-4e5b-b614-732176270bed/?size=200x200",
+      status: "none",
+      code: "AbcDef",
+      capacity: 50,
+      departureDate: "2020-10-23",
+    },
+    {
+      id: "e9890ca4-2bac-4e5b-b614-732176270bac",
+      img: "https://robohash.org/e9890ca4-2bac-4e5b-b614-732176270bac/?size=200x200",
+      status: "none",
+      code: "AbcDef",
+      capacity: 50,
+      departureDate: "2020-10-23",
+    },
+    {
+      id: "e9890ca4-2bac-4e5b-b614-732176270bee",
+      img: "https://robohash.org/e9890ca4-2bac-4e5b-b614-732176270bee/?size=200x200",
+      status: "none",
+      code: "AbcDef",
+      capacity: 50,
+      departureDate: "2020-10-23",
+    },
+    {
+      id: "e9890ca4-2bac-4e5b-b614-732176270bfcaa",
+      img: "https://robohash.org/e9890ca4-2bac-4e5b-b614-732176270bfcaa/?size=200x200",
+      status: "none",
+      code: "AbcDef",
+      capacity: 50,
+      departureDate: "2020-10-23",
+    },
+    {
+      id: "e9890ca4-2bac-4e5b-b614-732176270badf",
+      img: "https://robohash.org/e9890ca4-2bac-4e5b-b614-732176270badf/?size=200x200",
+      status: "none",
+      code: "AbcDef",
+      capacity: 50,
+      departureDate: "2020-10-23",
+    },
+  ],
+};
